c18f114c3cf2aa0b3d4b79e22dab98d5ea1b63b8
Дмитрий Троицкий <dimatroickij@yandex.ru>
2020-10-08 00:33:59 +0300

    Исправлена работа программы

--- genFile.py
+++ genFile.py
@@ -1,23 +1,36 @@
 import csv
-import gc
-import json
-import random
+from datetime import datetime
 
 from genPeople import UIP
-#
-# # with open('person.csv', "w", newline='', encoding='UTF-8') as csv_file:
-# #     writer = csv.writer(csv_file, delimiter='|')
-# #
-# #     for i in range(0, 200):
-# #         people = UIP(gender=1, ageMin=18, ageMax=20, capacity=1, sys_elector_id=7778613 + 1, elector_id=2289257 + i,
-# #                      elector_doc_id=6000 + i, elector_residence_id=14723 + i,
-# #                      residence_address_id=144082079413834240000345981)
-# #         writer.writerow(people.getElector_file())
-# #         del people
-# #         print('%i / 1000' % i)
-for i in range(0, 2000):
-    people = UIP(gender=1, ageMin=18, ageMax=20, capacity=1, sys_elector_id=7778613 + 1, elector_id=2289257 + i,
-                 elector_doc_id=6000 + i, elector_residence_id=14723 + i,
-                 residence_address_id=144082079413834240000345981)
 
-    print('%i / 1000' % i)
+count = 10
+date_start = datetime.now().timestamp()
+with open('D:\\elector.csv', "w", newline='', encoding='UTF-8') as people_file:
+    with open('D:\\elector_kind.csv', "w", newline='', encoding='UTF-8') as kind_file:
+        with open('D:\\elector_doc.csv', "w", newline='', encoding='UTF-8') as doc_file:
+            with open('D:\\elector_residence.csv', "w", newline='', encoding='UTF-8') as residence_file:
+                with open('D:\\elector_change_log.csv', "w", newline='', encoding='UTF-8') as change_log_file:
+                    csv_people = csv.writer(people_file, delimiter='|')
+                    csv_kind = csv.writer(kind_file, delimiter='|')
+                    csv_doc = csv.writer(doc_file, delimiter='|')
+                    csv_residence = csv.writer(residence_file, delimiter='|')
+                    csv_change_log = csv.writer(change_log_file, delimiter='|')
+
+                    for i in range(1, count + 1):
+                        people = UIP(gender=1, ageMin=18, ageMax=20, capacity=1, sys_elector_id=77786132 + i,
+                                     elector_id=22892527 + i, elector_change_log_id = 7662 + i,
+                                     elector_kind_id = 6450 + i, elector_doc_id=62000 + i,
+                                     elector_residence_id=222723 + i, residence_address_id=144082079413834240000345981,
+                                     address_id=1)
+                        csv_people.writerow(people.getElector())
+                        #[self.elector_kind_id, self.sys_elector_id, self.create_date, self.elector_kinds_json,
+                #self.has_external_vision]
+                        # kind_file.write(str(people.elector_kind_id) + '|' + str(people.sys_elector_id) + '|' +
+                        #                 people.create_date + '|' + people.elector_kinds_json + '|' +
+                        #                 str(people.has_external_vision) + '\n')
+                        csv_kind.writerow(people.getElector_kind())
+                        csv_doc.writerow(people.getElector_doc())
+                        csv_residence.writerow(people.getElector_residence())
+                        csv_change_log.writerow(people.getElector_change_log())
+
+print((datetime.now().timestamp() - date_start))
--- genPeople.py
+++ genPeople.py
@@ -1,4 +1,3 @@
-import gc
 import json
 import random
 from datetime import datetime, timedelta
@@ -26,51 +25,50 @@ from datetime import datetime, timedelta
 # 4.а) мужской пол: 3_520_000 (gender=1, ageMin=18, ageMax=90, capacity=2, doc_code=10)
 # 4.б) женский пол: 4_130_000 (gender=2, ageMin=18, ageMax=90, capacity=2, doc_code=10)
 
-def gen_datetime(ageMin, ageMax):
-    start = datetime(2020 - ageMin, datetime.now().month, datetime.now().day, 00, 00, 00)
-    end = datetime(2020 - ageMax - 1, datetime.now().month, datetime.now().day, 00, 00, 00) + timedelta(days=20)
-    return end + timedelta(days=random.randint(0, (start - end).days))
-
-
-def gen_doc_issue_date(birth):
-    age = int((datetime.now() - birth).days / 365.2425)
-
-    if age < 14:
-        return birth + timedelta(days=10)
-    elif age >= 14 and age < 20:
-        return birth + timedelta(days=14 * 365 + 4 + 15)  # 4 - возможное количество високосных годов
-    elif age >= 20 and age < 45:
-        return birth + timedelta(days=20 * 365 + 5 + 15)  # 5 - возможное количество високосных годов
-    else:
-        return birth + timedelta(days=458365 + 12 + 15)  # 12 - возможное количество високосных годов
-
-
-def gen_doc_number(type):
-    if type == 0:
-        randSer2 = str(random.randint(1, 99))
-        series = str(random.randint(10, 80)) + ' ' + '0' * (2 - len(randSer2)) + randSer2
-        number = str(random.randint(1, 999999))
-        number = '0' * (6 - len(number)) + number
-    elif type == 1:
-        alphabet = 'АВЕКМНОРСТХ'
-        romanNum = ['I', 'V', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
-        number = str(random.randint(1, 999999))
-        number = '0' * (6 - len(number)) + number
-        series = romanNum[random.randint(0, 9)] + '-T' + alphabet[random.randint(0, 10)]
-    else:
-        number = ''
-        series = ''
-    return [series, number]
 
 class UIP:
+    def gen_datetime(self, ageMin, ageMax):
+        start = datetime(2020 - ageMin, datetime.now().month, datetime.now().day, 00, 00, 00)
+        end = datetime(2020 - ageMax - 1, datetime.now().month, datetime.now().day, 00, 00, 00) + timedelta(days=20)
+        return end + timedelta(days=random.randint(0, (start - end).days))
+
+    def gen_doc_issue_date(self, birth):
+        age = int((datetime.now() - birth).days / 365.2425)
+
+        if age < 14:
+            return birth + timedelta(days=10)
+        elif age >= 14 and age < 20:
+            return birth + timedelta(days=14 * 365 + 4 + 15)  # 4 - возможное количество високосных годов
+        elif age >= 20 and age < 45:
+            return birth + timedelta(days=20 * 365 + 5 + 15)  # 5 - возможное количество високосных годов
+        else:
+            return birth + timedelta(days=458365 + 12 + 15)  # 12 - возможное количество високосных годов
+
+    def gen_doc_number(srlf, type):
+        if type == 0:
+            randSer2 = str(random.randint(1, 99))
+            series = str(random.randint(10, 80)) + ' ' + '0' * (2 - len(randSer2)) + randSer2
+            number = str(random.randint(1, 999999))
+            number = '0' * (6 - len(number)) + number
+        elif type == 1:
+            alphabet = 'АВЕКМНОРСТХ'
+            romanNum = ['I', 'V', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+            number = str(random.randint(1, 999999))
+            number = '0' * (6 - len(number)) + number
+            series = romanNum[random.randint(0, 9)] + '-T' + alphabet[random.randint(0, 10)]
+        else:
+            number = ''
+            series = ''
+        return [str(series), str(number)]
+
     def gen_name(self, sex):
         return random.choice(self.data['names'][sex])
 
     def gen_surname(self, sex):
         return random.choice(self.data['surnames'][sex])
 
-    def __init__(self, sys_elector_id, gender, ageMin, ageMax, capacity, elector_id, elector_doc_id,
-                 elector_residence_id, residence_address_id):
+    def __init__(self, sys_elector_id, gender, ageMin, ageMax, capacity, elector_id, elector_doc_id, address_id,
+                 elector_residence_id, residence_address_id, elector_change_log_id, elector_kind_id):
         self.data = None
         with open("person.json", "r", encoding='UTF-8') as read_file:
             self.data = json.load(read_file)
@@ -80,100 +78,136 @@ class UIP:
         # Суффиксы отчества в зависимоти от пола
         genderSuffix = {1: 'ович', 2: 'овна'}
         # Словарь типов документа
-        docList = {0: [10, 9], 1: [123456789, 5]}
+        docList = {0: ['10', '9'], 1: ['123456789', '5']}
 
         # Генерация даты рождения в завсимости от возраста
-        birth = gen_datetime(ageMin, ageMax)
+        birth = self.gen_datetime(ageMin, ageMax)
 
         # Данные для таблицы elector
         self.elector_id = elector_id  # PK из таблицы, но будет устанавливаться вручную, при этом надо поддерживать соответствие с последовательностями из базы
+        self.sys_elector_id = sys_elector_id  # НЕТ ИНФОРМАЦИИ О СПОСОБЕ ГЕНЕРАЦИИ ПОЛЯ (пока приходит готовое) sys_elector_id
         self.last_name = self.gen_surname(genderList[gender])
         self.first_name = self.gen_name(genderList[gender])
         self.middle_name = self.gen_name(genderList[gender]) + genderSuffix[gender]
         self.birth_day = birth.strftime('%Y-%m-%d')
         self.birth_day_string = birth.strftime('%Y.%m.%d')
+        self.is_birth_day_year_only = False
         self.gender_id = gender
         self.capacity_id = capacity
-        self.country_id = 643
-        self.start_date = birth.strftime('%Y-%m-%d') + ' 00:00:00'  # для таблицы elector
-        # НЕТ ИНФОРМАЦИИ О СПОСОБЕ ГЕНЕРАЦИИ ПОЛЯ (пока приходит готовое) sys_elector_id
-        self.sys_elector_id = sys_elector_id
         self.address_id = 1
+        self.country_id = '643'
+        self.snils = self.inn = None
+        self.start_date = birth.strftime('%Y-%m-%d') + ' 00:00:00'  # для таблицы elector
+        self.end_date = None
+        self.input_source = '1'
+        self.is_actual = True
+        self.kca = 1
+        self.capacity_actual_date = None
+
+        # Данные для таблицы elector_kind
+        self.elector_kind_id = elector_kind_id
+        # sys_elector_id
+        self.create_date = self.start_date2 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.elector_kinds_json = json.dumps([{'id': 1}])
+        self.has_external_vision = False
 
         # Данные для таблицы elector_doc
-        self.elector_doc_id = elector_doc_id  # PK из таблицы, но будет устанавливаться вручную, при этом надо поддерживать соответствие с последовательностями из базы
-        # НЕПОНЯТНО КАК МЕНЯТЬ (ТАК КАК У КАЖДОГО УИП СВОЕ МЕСТО ВЫДАЧИ ДОКУМЕНТА). Сейчас одинаковое значение:
-        self.subject_global_id = 123
-
+        self.elector_doc_id = elector_doc_id  # надо поддерживать соответствие с последовательностями из базы
+        # sys_elector_id
+        self.subject_global_id = 123  # НЕПОНЯТНО КАК МЕНЯТЬ (ТАК КАК У КАЖДОГО УИП СВОЕ МЕСТО ВЫДАЧИ ДОКУМЕНТА)
         if ageMax < 14:
             doc_code = 1
         else:
             doc_code = 0
         self.elector_doc_code_id = docList[doc_code][0]
-        doc_data = gen_doc_number(doc_code)
+        doc_data = self.gen_doc_number(doc_code)
         self.elect_doc_series = doc_data[0]
         self.elect_doc_number = doc_data[1]
-        self.start_date2 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # для таблицы elector_doc
-
-        self.elect_doc_issue_date = gen_doc_issue_date(birth).strftime('%Y-%m-%d')
+        # input_source_id
+        # start_date_2
+        # self.end_date
+        # is_actual
+        # kca
+        self.elect_doc_issue_date = self.gen_doc_issue_date(birth).strftime('%Y-%m-%d')
         self.elector_doc_type_id = docList[doc_code][1]
 
         # Данные для таблицы elector_residence
         self.elector_residence_id = elector_residence_id  # PK из таблицы
-        self.elector_residence_kind_id = 1
+        # sys_elector_id
         # НЕПОНЯТНО КАК МЕНЯТЬ (ТАК КАК У КАЖДОГО УИП СВОЕ МЕСТО ЖИТЕЛЬСТВА). Сейчас постоянное значение
         self.residence_address_id = residence_address_id
+        self.arrival_address_id = self.departure_address_id = self.temp_start_date = self.temp_end_date = None
+        # input_source
+        self.elector_residence_kind_id = 1
+        self.start_date3 = self.start_date2[0:10] + ' 00:00:00'
+        # end_date
+        # is_actual
+        # kca
+        self.arrive_prefered_date = self.temp_doc_number = None
+        self.is_create_arrive_needed = False
 
         # Данные для таблицы elector_change_log
         # НЕИЗВЕСТНО ЗНАЧЕНИЕ, но вроде везде 1
+        self.elector_change_log_id = elector_change_log_id
+        # elector_id
+        # elector_doc_id
+        # elector_residence_id
+        self.elector_zags_info_id = None
+        # sys_elector_id
         self.change_type_id = 1
         self.change_number = 0
         self.change_date = self.birth_day + ' 00:00:00'
+        # is_actual
+        # start_date2
+        self.create_user_id = None
+        # input_source
+        # kca
         self.change_basis_id = 85  # 89 - листок прибытия, 85 - рождение, 97 - листок убытия
-        self.kca = 1
-        # БУДЕТ БРАТЬСЯ ИЗ ID СОЗДАННОГО person: self.elector_id = ''
-        # БУДЕТ БРАТЬСЯ ИЗ ID СОЗДАННОГО документа self.elector_doc_id
-        # БУДЕТ БРАТЬСЯ ИЗ ID СОЗДАННОГО elector_residence self.elector_residence_id
-
-        # input_source = 1
-        self.input_source = 1
+        self.is_death = False
+        self.is_deleted = False
+        self.import_log_id = None
+        self.is_approved = True
+        # start_date2
+        self.update_user_id = None
+        self.is_departured_in_military = False
+        self.is_departured_from_addr = False
+        self.is_departured_in_prison = False
+        self.kind_names = 'избиратель'
 
     def getElector(self):
-        return [self.last_name, self.first_name, self.middle_name, self.birth_day, self.birth_day_string,
-                self.gender_id, self.capacity_id, self.country_id, self.start_date, self.input_source,
-                self.sys_elector_id, self.address_id]
-
-    def getElector_file(self):
-        return [str(self.elector_id), str(self.sys_elector_id), self.last_name, self.first_name, self.middle_name,
-                self.birth_day, self.birth_day_string, 'f', str(self.gender_id), str(self.capacity_id),
-                str(self.address_id), str(self.country_id), None, None, self.start_date, None, str(self.input_source),
-                't', None, '']
+        return [self.elector_id, self.sys_elector_id, self.last_name, self.first_name, self.middle_name,
+                self.birth_day, self.birth_day_string, self.is_birth_day_year_only, self.gender_id, self.capacity_id,
+                self.address_id, self.country_id, self.snils, self.inn, self.start_date, self.end_date,
+                self.input_source, self.is_actual, self.kca, self.capacity_actual_date]
 
     def setElector_id(self, elector_id):
         self.elector_id = elector_id
 
     def getElector_kind(self):
-        return [self.sys_elector_id, self.start_date2, '[{"id": 1}]']
-
-    def getElector_kind_file(self):
-        return [str(self.sys_elector_id), self.start_date2, '[{"id": 1}]']
+        return [self.elector_kind_id, self.sys_elector_id, self.create_date, self.elector_kinds_json,
+                self.has_external_vision]
 
     def getElector_doc(self):
-        return [self.sys_elector_id, self.subject_global_id, self.elector_doc_code_id, self.elect_doc_series,
-                self.elect_doc_number, self.input_source, self.start_date2, self.elect_doc_issue_date,
-                self.elector_doc_type_id]
+        return [self.elector_doc_id, self.sys_elector_id, self.subject_global_id, self.elector_doc_code_id,
+                self.elect_doc_series, self.elect_doc_number, self.input_source, self.start_date2, self.end_date,
+                self.is_actual, self.kca, self.elect_doc_issue_date, self.elector_doc_type_id]
 
     def setElector_doc_id(self, elector_doc_id):
         self.elector_doc_id = elector_doc_id
 
     def getElector_residence(self):
-        return [self.sys_elector_id, self.residence_address_id, self.input_source, self.elector_residence_kind_id,
-                self.start_date2[0:10] + ' 00:00:00']
+        return [self.elector_residence_id, self.sys_elector_id, self.residence_address_id, self.arrival_address_id,
+                self.departure_address_id, self.temp_start_date, self.temp_end_date, self.input_source,
+                self.elector_residence_kind_id, self.start_date3, self.end_date, self.is_actual, self.kca,
+                self.arrive_prefered_date, self.is_create_arrive_needed, self.temp_doc_number]
 
     def setElector_residence_id(self, elector_residence_id):
         self.elector_residence_id = elector_residence_id
 
     def getElector_change_log(self):
-        return [self.elector_id, self.elector_doc_id, self.elector_residence_id, self.sys_elector_id,
-                self.change_type_id, self.change_number, self.change_date, self.start_date2, self.kca,
-                self.change_basis_id, self.input_source]
+        return [self.elector_change_log_id, self.elector_id, self.elector_doc_id, self.elector_residence_id,
+                self.elector_zags_info_id, self.sys_elector_id, self.change_type_id, self.change_number,
+                self.change_date, self.is_actual, self.start_date2, self.create_user_id, self.input_source, self.kca,
+                self.change_basis_id, self.is_death, self.is_deleted, self.import_log_id, self.is_approved,
+                self.start_date2, self.update_user_id, self.is_departured_in_military, self.is_departured_from_addr,
+                self.is_departured_in_prison, 'избиратель']
