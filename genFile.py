9d5a2b109a128ac743f857e915f1d27e2225b99b
Троицкий Дмитрий Александрович <d.troickiy@voskhod.ru>
2020-10-09 11:30:42 +0300

    Поправлены ошибки, настроено для создания данных на 2 процесса по 1_000_000

--- genFile.py
+++ genFile.py
@@ -10,14 +10,16 @@ from genPeople import UIP
 def saveFile(count, number):
     print('start %s' % number)
     for i in range(1, count + 1):
-        with open('D:\\genFileGAS_Vybory\elector%s.csv' % number, "a", newline='', encoding='UTF-8') as people_file:
-            with open('D:\\genFileGAS_Vybory\elector_kind%s.csv' % number, "a", newline='',
+        if i % 1000 == 0:
+            print(str(i) + ' / 1_000_000')
+        with open('F:\\genFileGAS_Vybory\elector%i.csv' % number, "a", newline='', encoding='UTF-8') as people_file:
+            with open('F:\\genFileGAS_Vybory\elector_kind%i.csv' % number, "a", newline='',
                       encoding='UTF-8') as kind_file:
-                with open('D:\\genFileGAS_Vybory\elector_doc%s.csv' % number, "a", newline='',
+                with open('F:\\genFileGAS_Vybory\elector_doc%i.csv' % number, "a", newline='',
                           encoding='UTF-8') as doc_file:
-                    with open('D:\\genFileGAS_Vybory\elector_residence%s.csv' % number, "a", newline='',
+                    with open('F:\\genFileGAS_Vybory\elector_residence%i.csv' % number, "a", newline='',
                               encoding='UTF-8') as residence_file:
-                        with open('D:\\genFileGAS_Vybory\elector_change_log%s.csv' % number, "a", newline='',
+                        with open('F:\\genFileGAS_Vybory\elector_change_log%i.csv' % number, "a", newline='',
                                   encoding='UTF-8') as change_log_file:
                             csv_people = csv.writer(people_file, delimiter='|')
                             csv_kind = csv.writer(kind_file, delimiter='|')
@@ -25,16 +27,19 @@ def saveFile(count, number):
                             csv_residence = csv.writer(residence_file, delimiter='|')
                             csv_change_log = csv.writer(change_log_file, delimiter='|')
 
-                            people = UIP(gender=1, ageMin=10, ageMax=13, capacity=1, sys_elector_id=77786132 + i,
-                                         elector_id=22892527 + i, elector_change_log_id=7662 + i,
-                                         elector_kind_id=6450 + i, elector_doc_id=62000 + i,
-                                         elector_residence_id=222723 + i)
+                            people = UIP(gender=1, ageMin=0, ageMax=13, capacity=1,
+                                         sys_elector_id=1_000_000_000 + i + count * (number - 1),
+                                         elector_id=4_000_000_000 + i + count * (number - 1),
+                                         elector_change_log_id=1_000_000 + i + count * (number - 1),
+                                         elector_kind_id=100_000 + i + count * (number - 1),
+                                         elector_doc_id=100_000 + i + count * (number - 1),
+                                         elector_residence_id=100_000 + i + count * (number - 1))
 
-                            print(people.getElector())
-                            print(people.getElector_kind())
-                            print(people.getElector_doc())
-                            print(people.getElector_residence())
-                            print(people.getElector_change_log())
+                            # print(people.getElector())
+                            # print(people.getElector_kind())
+                            # print(people.getElector_doc())
+                            # print(people.getElector_residence())
+                            # print(people.getElector_change_log())
 
                             csv_people.writerow(people.getElector())
                             csv_kind.writerow(people.getElector_kind())
@@ -46,13 +51,13 @@ def saveFile(count, number):
 
 
 if __name__ == '__main__':
-    count = 1
-    countProcess = 1
+    count = 1_000_000
+    countProcess = 2
 
     startWork = datetime.now().timestamp()
     procs = []
     for i in range(1, countProcess + 1):
-        proc = Process(target=saveFile, args=(count, '%i' % i))
+        proc = Process(target=saveFile, args=(count, i))
         procs.append(proc)
         proc.start()
 
@@ -64,3 +69,4 @@ if __name__ == '__main__':
     avgDay = avgHour / 24
     print('Средняя скорость создания записей: %f в секунду' % total)
     print('Среднее время создания 150_000_000 записей: %f часов или %f дней' % (avgHour, avgDay))
+    print(datetime.now())
