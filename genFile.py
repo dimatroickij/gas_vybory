b3f86425f07c8cf30a4c41bdee305d5380809666
Дмитрий Троицкий <dimatroickij@yandex.ru>
2020-11-02 15:01:59 +0300

    Улкчшена производительность программы и код исправлен под новую схему БД

--- genFile.py
+++ genFile.py
@@ -2,13 +2,24 @@ import csv
 import json
 from datetime import datetime
 from multiprocessing import Process
-from threading import Thread
-
 from genPeople import UIP
 
 
 def saveFile(count, number):
     print('start %s' % number)
+    peopleFile = None
+    with open("person.json", "r", encoding='UTF-8') as read_file:
+        peopleFile = json.load(read_file)
+
+    residenceFile = None
+    with open("residence_id.csv", "r", encoding='UTF-8') as read_file:
+        residence = csv.reader(read_file, delimiter='|')
+        residenceFile = list(residence)[0]
+
+    subjectFile = None
+    with open("subject_id.json", "r", encoding='UTF-8') as read_file:
+        subjectFile = json.load(read_file)
+
     for i in range(1, count + 1):
         with open('D:\\genFileGAS_Vybory\elector%s.csv' % number, "a", newline='', encoding='UTF-8') as people_file:
             with open('D:\\genFileGAS_Vybory\elector_kind%s.csv' % number, "a", newline='',
@@ -25,16 +36,17 @@ def saveFile(count, number):
                             csv_residence = csv.writer(residence_file, delimiter='|')
                             csv_change_log = csv.writer(change_log_file, delimiter='|')
 
-                            people = UIP(gender=1, ageMin=10, ageMax=13, capacity=1, sys_elector_id=77786132 + i,
+                            people = UIP(ageMin=18, ageMax=80, capacity=1, sys_elector_id=77786132 + i,
                                          elector_id=22892527 + i, elector_change_log_id=7662 + i,
                                          elector_kind_id=6450 + i, elector_doc_id=62000 + i,
-                                         elector_residence_id=222723 + i)
+                                         elector_residence_id=222723 + i, peopleFile=peopleFile,
+                                         residenceFile=residenceFile, subjectFile=subjectFile)
 
-                            print(people.getElector())
-                            print(people.getElector_kind())
-                            print(people.getElector_doc())
-                            print(people.getElector_residence())
-                            print(people.getElector_change_log())
+                            # print(people.getElector())
+                            # print(people.getElector_kind())
+                            # print(people.getElector_doc())
+                            # print(people.getElector_residence())
+                            # print(people.getElector_change_log())
 
                             csv_people.writerow(people.getElector())
                             csv_kind.writerow(people.getElector_kind())
@@ -44,10 +56,9 @@ def saveFile(count, number):
     print('end process %s' % number)
 
 
-
 if __name__ == '__main__':
-    count = 1
-    countProcess = 1
+    count = 10_000
+    countProcess = 3
 
     startWork = datetime.now().timestamp()
     procs = []
@@ -59,8 +70,8 @@ if __name__ == '__main__':
     for proc in procs:
         proc.join()
 
-    total = count * countProcess  / (datetime.now().timestamp() - startWork)
-    avgHour = 150_000_000 / total / 60/ 60
+    total = count * countProcess / (datetime.now().timestamp() - startWork)
+    avgHour = 150_000_000 / total / 60 / 60
     avgDay = avgHour / 24
     print('Средняя скорость создания записей: %f в секунду' % total)
     print('Среднее время создания 150_000_000 записей: %f часов или %f дней' % (avgHour, avgDay))
--- genPeople.py
+++ genPeople.py
@@ -28,22 +28,15 @@ from datetime import datetime, timedelta
 
 
 class UIP:
-    def __init__(self, sys_elector_id, gender, ageMin, ageMax, capacity, elector_id, elector_doc_id,
-                 elector_residence_id, elector_change_log_id, elector_kind_id):
-        self.data = None
-        with open("person.json", "r", encoding='UTF-8') as read_file:
-            self.data = json.load(read_file)
+    def __init__(self, sys_elector_id, ageMin, ageMax, capacity, elector_id, elector_doc_id, elector_residence_id,
+                 elector_change_log_id, elector_kind_id, peopleFile, residenceFile, subjectFile):
+        self.peopleFile = peopleFile
 
         # Список адресов регистрации
-        self.residence = None
-        with open("residence_id.csv", "r", encoding='UTF-8') as read_file:
-            residence = csv.reader(read_file, delimiter='|')
-            self.residence = list(residence)[0]
+        self.residence = residenceFile
 
         # Список мест выдачи паспорта
-        self.subject = None
-        with open("subject_id.json", "r", encoding='UTF-8') as subject_file:
-            self.subject = json.load(subject_file)
+        self.subject = subjectFile
 
         # Словарь пола
         genderList = {1: 'male', 2: 'female'}
@@ -54,6 +47,8 @@ class UIP:
 
         # Генерация даты рождения в завсимости от возраста
         birth = self.gen_datetime(ageMin, ageMax)
+
+        gender = random.randint(1, 2)
 
         # Данные для таблицы elector
         # PK из таблицы, но будет устанавливаться вручную, при этом надо поддерживать соответствие с последовательностями из базы
@@ -76,6 +71,9 @@ class UIP:
         self.is_actual = True
         self.kca = 1
         self.capacity_actual_date = None
+        self.imanid = None
+        self.ik_id_array = None
+        self.is_public_version = False
 
         # Данные для таблицы elector_kind
         self.elector_kind_id = elector_kind_id
@@ -144,6 +142,10 @@ class UIP:
         self.is_departured_from_addr = False
         self.is_departured_in_prison = False
         self.kind_names = 'избиратель'
+        self.is_checked_ukd = False
+        self.is_valid_ukd = False
+        self.is_trusted = False
+
 
     def gen_datetime(self, ageMin, ageMax):
         start = datetime(2020 - ageMin, datetime.now().month, datetime.now().day, 00, 00, 00)
@@ -173,17 +175,18 @@ class UIP:
             romanNum = ['I', 'V', 'L', 'X', 'C']
             number = str(random.randint(1, 999999))
             number = '0' * (6 - len(number)) + number
-            series = romanNum[random.randint(0, 5)] + '-' + alphabet[random.randint(0, 10)] + alphabet[random.randint(0, 10)]
+            series = romanNum[random.randint(0, 4)] + '-' + alphabet[random.randint(0, 10)] + alphabet[
+                random.randint(0, 10)]
         else:
             number = ''
             series = ''
         return [str(series), str(number)]
 
     def gen_name(self, sex):
-        return random.choice(self.data['names'][sex])
+        return random.choice(self.peopleFile['names'][sex])
 
     def gen_surname(self, sex):
-        return random.choice(self.data['surnames'][sex])
+        return random.choice(self.peopleFile['surnames'][sex])
 
     def genResidenceAddressFile(self):
         keys = []
@@ -209,10 +212,11 @@ class UIP:
             json.dump(keys, subject_id_file)
 
     def getElector(self):
-        return [self.elector_id, self.sys_elector_id, self.last_name, self.first_name, self.middle_name,
-                self.birth_day, self.birth_day_string, self.is_birth_day_year_only, self.gender_id, self.capacity_id,
-                self.address_id, self.country_id, self.snils, self.inn, self.start_date, self.end_date,
-                self.input_source, self.is_actual, self.kca, self.capacity_actual_date]
+        return [self.last_name, self.first_name, self.middle_name, self.birth_day, self.birth_day_string,
+                self.is_birth_day_year_only, self.gender_id, self.capacity_id, self.country_id, self.snils, self.inn,
+                self.start_date, self.end_date, self.input_source, self.is_actual, self.kca, self.capacity_actual_date,
+                self.elector_id, self.imanid, self.sys_elector_id, self.address_id, self.ik_id_array,
+                self.is_public_version]
 
     def setElector_id(self, elector_id):
         self.elector_id = elector_id
@@ -222,26 +226,29 @@ class UIP:
                 self.has_external_vision]
 
     def getElector_doc(self):
-        return [self.elector_doc_id, self.sys_elector_id, self.subject_global_id, self.elector_doc_code_id,
-                self.elect_doc_series, self.elect_doc_number, self.input_source, self.start_date2, self.end_date,
-                self.is_actual, self.kca, self.elect_doc_issue_date, self.elector_doc_type_id]
+        return [self.subject_global_id, self.elector_doc_code_id, self.elect_doc_series, self.elect_doc_number,
+                self.input_source, self.start_date2, self.end_date, self.is_actual, self.kca, self.elect_doc_issue_date,
+                self.elector_doc_type_id, self.elector_doc_id, self.sys_elector_id, self.ik_id_array,
+                self.is_public_version]
 
     def setElector_doc_id(self, elector_doc_id):
         self.elector_doc_id = elector_doc_id
 
     def getElector_residence(self):
-        return [self.elector_residence_id, self.sys_elector_id, self.residence_address_id, self.arrival_address_id,
-                self.departure_address_id, self.temp_start_date, self.temp_end_date, self.input_source,
-                self.elector_residence_kind_id, self.start_date3, self.end_date, self.is_actual, self.kca,
-                self.arrive_prefered_date, self.is_create_arrive_needed, self.temp_doc_number]
+        return [self.temp_start_date, self.temp_end_date, self.input_source, self.elector_residence_kind_id,
+                self.start_date3, self.end_date, self.is_actual, self.kca, self.arrive_prefered_date,
+                self.is_create_arrive_needed, self.temp_doc_number, self.elector_residence_id, self.sys_elector_id,
+                self.residence_address_id, self.arrival_address_id, self.departure_address_id, self.ik_id_array,
+                self.is_public_version]
 
     def setElector_residence_id(self, elector_residence_id):
         self.elector_residence_id = elector_residence_id
 
     def getElector_change_log(self):
-        return [self.elector_change_log_id, self.elector_id, self.elector_doc_id, self.elector_residence_id,
-                self.elector_zags_info_id, self.sys_elector_id, self.change_type_id, self.change_number,
-                self.change_date, self.is_actual, self.start_date2, self.create_user_id, self.input_source, self.kca,
-                self.change_basis_id, self.is_death, self.is_deleted, self.import_log_id, self.is_approved,
-                self.start_date2, self.update_user_id, self.is_departured_in_military, self.is_departured_from_addr,
-                self.is_departured_in_prison, 'избиратель']
+        return [self.elector_zags_info_id, self.change_type_id, self.change_number, self.change_date, self.is_actual,
+                self.start_date2, self.create_user_id, self.input_source, self.kca, self.change_basis_id, self.is_death,
+                self.is_deleted, self.import_log_id, self.is_approved, self.start_date2, self.update_user_id,
+                self.is_departured_in_military, self.elector_change_log_id, self.elector_id, self.elector_doc_id,
+                self.elector_residence_id, self.elector_zags_info_id, self.sys_elector_id, self.is_departured_from_addr,
+                self.is_departured_in_prison, self.kind_names, self.is_checked_ukd, self.is_valid_ukd, self.is_trusted,
+                self.ik_id_array, self.is_public_version]
